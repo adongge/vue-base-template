0ec72864a3eddeed5ede24cd3458b5fc507d32c5
adong <adddge@sohu.com>
2021-11-12 16:37:25 +0800

    update

---
+++ template/src/util/common.js
@@ -0,0 +1,100 @@
+import store from '@/store/index'
+export default {
+    //cookie 操作
+    //s20是代表20秒
+    //h是指小时，如12小时则是：h12
+    //d是天数，30天则：d30
+    //setCookie("key","value","s20");
+    getCookie(name) {
+        var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+        arr = document.cookie.match(reg)
+        if(arr) {
+            return unescape(arr[2]);
+        } else {
+            return null;
+        }
+    },
+    setCookie(name, value, time) {
+        var strsec = this.getsec(time);
+        var exp = new Date();
+        exp.setTime(exp.getTime() + strsec * 1);
+        document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString() + ";path=/";
+    },
+    delCookie(name) {
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval = this.getCookie(name);
+        if (cval != null) {
+            document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString() + ";path=/";
+        }
+    },
+    getsec(str) {
+        if (str == undefined) str = 'd1';
+        var str1 = str.substring(1, str.length) * 1;
+        var str2 = str.substring(0, 1);
+        if (str2 == "s") {
+            return str1 * 1000;
+        } else if (str2 == "h") {
+            return str1 * 60 * 60 * 1000;
+        } else if (str2 == "d") {
+            return str1 * 24 * 60 * 60 * 1000;
+        }
+    },
+    loading(){
+        store.dispatch('partLoading');
+    },
+    isMobile() {
+        let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+        return flag;
+    },
+    deepClone(obj) {// 深度拷贝
+        let o = obj instanceof Array ? [] : {};
+        for (let k in obj){
+            o[k] = typeof obj[k] === Object ? this.deepClone(obj[k]) : obj[k];
+        }  
+        return o;
+    },
+    inArray(search,array) {
+        for (var i in array) {
+            if (array[i] == search) {
+                return true;
+            }
+        }
+        return false;
+    },
+    delValueArray(value,arr){
+        for (let index = 0; index < arr.length; index++) {
+            if(arr[index] == value){
+                arr = this.delArray(index,arr);
+            }
+        }
+        return arr;
+    },
+    delArray(n,arr){
+        if(n<0){
+            return arr;
+        }else{
+            return arr.slice(0,n).concat(arr.slice(n+1,arr.length));
+        }
+    },
+    // 时间格式化
+    formatDate(date, fmt) {
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+        }
+        let o = {
+            'M+': date.getMonth() + 1,
+            'd+': date.getDate(),
+            'h+': date.getHours(),
+            'm+': date.getMinutes(),
+            's+': date.getSeconds()
+        };
+        for (let k in o) {
+            if (new RegExp(`(${k})`).test(fmt)) {
+                let str = o[k] + '';
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : ('00'+str).substr(str.length));
+            }
+        }
+        return fmt;
+    }
+}
---
+++ template/src/util/http.js
@@ -0,0 +1,150 @@
+/**
+ * 封装统一的ajax请求
+ */
+import axios from 'axios'
+
+import store from '@/store/index'
+import { Message } from 'element-ui';
+import common from '@/util/common'
+
+/**
+ * axios请求拦截器
+ * @param {object} config axios请求配置对象
+ * @return {object} 请求成功或失败时返回的配置对象或者promise error对象
+ **/
+
+axios.interceptors.request.use(config => { 
+  if(store.state.common.partLoading == false)
+    store.dispatch('showLoading');
+  config.headers['Content-Type'] = 'application/json; charset=utf-8';
+  config.headers['X-Requested-With'] = 'XMLHttpRequest';
+  if(store.state.user.token){
+    config.headers['Authorization'] = `Bearer ${store.state.user.token}`;
+  }
+  return config
+}, error => {
+  store.dispatch('closeLoading')
+  return Promise.reject(error)
+})
+
+/**
+ * axios 响应拦截器
+ * @param {object} response 从服务端响应的数据对象或者error对象
+ * @return {object} 响应成功或失败时返回的响应对象或者promise error对象
+ **/
+axios.interceptors.response.use(response => {
+  store.dispatch('closeLoading')
+  return response
+}, error => {
+  if (error.code === 'ECONNABORTED'){
+    Message.closeAll();
+    Message.error('请求超时，请重新尝试！')
+  }
+  store.dispatch('closeLoading')
+  return Promise.resolve(error)
+})
+
+/**
+ * 请求发出后检查返回的状态码,统一捕获正确和错误的状态码，正确就直接返回response,错误就自定义一个返回对象
+ * @param {object} response 响应对象
+ * @return {object} 响应正常就返回响应数据否则返回错误信息
+ **/
+function checkStatus (response) {
+  // 如果状态码正常就直接返回数据,响应正确，code === 0
+  if ( response && ( response.code === 0 ) ) {
+    return response; // 直接返回http response响应的data,此data会后端返回的数据数据对象，包含后端自定义的code,message,data属性
+  }else{
+    let res = response.data||response;
+    if(typeof(res)=='string'){
+      try {
+        res = JSON.parse(res)
+      } catch (error) {
+        console.log('返回错误'+error)
+      }
+    }
+    if(res.msg == 'Signature has expired' || res.code == 1001 ){
+      res.msg = '用户过期';
+      common.delCookie('_KPRDATA');
+      store.commit('setToken',undefined);
+      window.location.href = '/#/login';
+    }
+    if(res.msg == 'Wrong number of segments' && common.getCookie('_KPRDATA') == null ){
+        window.location.href = '/#/login';
+    }
+    let msg = res;
+    if(res.msg != undefined) msg = res.msg;
+    else msg = '未知错误';
+    if(msg.length>256){
+      console.error(msg);
+      msg = '服务错误，请联系管理员！';
+    }
+    if(res.code == 1003){
+      msg = JSON.stringify(res.msg);
+    }
+    Message.closeAll();
+    Message.error(msg);
+    console.error(res);
+  }
+  throw response;//抛出异常不继续往下走
+}
+
+axios.defaults.baseURL = '/';
+axios.defaults.timeout = 60000;
+
+export default {
+  post (url, data) {
+    return axios({
+      method: 'post',
+      url: url,
+      data: data,
+    }).then((res) => {
+
+      let result = res.response || res.data;
+
+      return checkStatus(result)
+    
+
+    })
+  },
+  get (url, params) {
+    return axios({
+      method: 'get',
+      url:url,
+      params,
+    }).then(
+      (res) => {
+        let result = res.response || res.data;
+        return checkStatus(result)
+      }
+    )
+  },
+  download(url,params){
+    return axios({
+        method:'post',
+        url:url,
+        data:params,
+        responseType: 'blob'
+    }).then((res) => {
+        return res.data;
+    }).catch(err => {
+        Message.closeAll();
+        Message.error(err);
+    })
+
+  },
+  upload(url,param){
+    return axios({
+      method: 'post',
+      url: url,
+      data:param,
+      onUploadProgress:function(e){//添加上传进度监听事件,展示上传进度
+          let complete = ((e.loaded / e.total * 100) | 0) + "%";
+          complete = "请勿关闭浏览器，已上传" + complete + " 等待响应结果... ";
+          store.commit('setLoadType',{type:'full',text:complete});
+      }
+    }).then((res) => {
+      let result = res.response || res.data;
+      return checkStatus(result)
+    })
+  }
+}
