0cb024cb2a7d5627234ec888157ab7fc5e009497
adong <adddge@sohu.com>
2021-11-12 20:26:39 +0800

    update

---
+++ template/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <loading id="loading" :show="this.$store.state.common.showLoading" ></loading>
+  </div>
+</template>
+
+<script>
+import loading from "@/components/Loading";//顶部 loading
+
+export default {
+  name: 'App',
+  components: {loading}
+}
+</script>
+
+<style>
+.el-loading-mask{position:fixed;left:0px;top:0px;width:100%;height:100%;}
+</style>
