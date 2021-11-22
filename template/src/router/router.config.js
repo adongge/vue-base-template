59ba12ff8bb8cb83516624d407de053f0589b916
adongge <814955615@qq.com>
2021-11-22 17:31:33 +0800

    update
    update

--- template/src/router/router.config.js
+++ template/src/router/router.config.js
@@ -1,6 +1,8 @@
 import Home from '../views/Home'
 import Login from '@/components/Login'
 import NotAuth from '@/components/NotAuth'
+import HelloWorld from '@/components/HelloWorld'
+
 
 const config = [
     {
@@ -8,21 +10,22 @@ const config = [
         name: 'Home',
         component: Home,
         children: [
-            // { path: '/user', name: 'user', component:User, meta:{ requireAuth: true, title:'参考' } }
+            // { path: '/user', name: 'user', component:User, meta:{ requireAuth: true, title:'参考' } },
+            { path: '/helloworld', name: 'helloworld', component:HelloWorld, meta:{ title:'参考' } },
+            {
+                path: '/about',
+                name: 'About',
+                // route level code-splitting
+                // this generates a separate chunk (about.[hash].js) for this route
+                // which is lazy-loaded when the route is visited.
+                component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+            },
         ]
     },
     {
         path: '/login',
         name: 'Login',
         component: Login
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     },
     {
         path: "/404",
